849b40072d72b73940d107671f369db4610d8d9b
Caleb Berkstresser <cdberkstresser@protonmail.com>
2021-02-18 05:56:39 +0000

    Adding cadence service

--- csc-speed-service/main.cpp
+++ csc-speed-service/main.cpp
@@ -19,12 +19,12 @@ int main(int argc, char *argv[])
 {
     // GPI BCM PIN 2
     const int WHEEL_SENSOR_PIN = 8;
+    // GPI BCM PIN 3
+    const int CRANK_SENSOR_PIN = 9;
     // CYCLE INTERVAL IN MILLISECONDS
     const int TIMER_REPORTING_INTERVAL = 5;
     // MAXIMUM TIME ALLOWED TO MANUALLY TRIP A NOTIFICATION UPDATE IN MILLISECONDS
     const int MAX_REPORTING_INTERVAL = 1000;
-    // REPORT REVOLUTIONS AND TIMESTAMP EVERY X REVOLUTIONS OF THE WHEEL
-    const int REVOLUTION_REPORTING_INTERVAL = 1; // one seems to be okay without overwhelming the receiver
     // SET UP WIRING PI CONNECTION
     wiringPiSetup();
 
@@ -85,20 +85,26 @@ int main(int argc, char *argv[])
     pinMode(WHEEL_SENSOR_PIN, INPUT);
 
     int lastWheelValue = digitalRead(WHEEL_SENSOR_PIN);
-    unsigned int numberOfRevolutions = 0;
-    unsigned int numberOfCranks = 0;
+    int lastCrankValue = digitalRead(CRANK_SENSOR_PIN);
+    unsigned int numberOfRevolutions = 1;
+    unsigned int numberOfCranks = 1;
     int wheelSensorValue;
+    int crankSensorValue;
+    unsigned short lowWheelMilliBit = 1;
+    unsigned short highWheelMilliBit = 0;
+    unsigned short lowCrankMilliBit = 1;
+    unsigned short highCrankMilliBit  =0;
     auto lastReportingTimeInMillis = std::chrono::system_clock::now();
-    const auto cyclingServiceProvider = [&service, &startMillis, &wheelSensorValue, &numberOfRevolutions, &lastWheelValue, &lastReportingTimeInMillis]() {
+    const auto cyclingServiceProvider = [&service, &startMillis, &wheelSensorValue, &crankSensorValue, &numberOfRevolutions, &numberOfCranks, &lastWheelValue, &lastCrankValue, &lastReportingTimeInMillis, &lowWheelMilliBit, &highWheelMilliBit, &lowCrankMilliBit, &highCrankMilliBit]() {
         int revsJustChangedFlag = 0;
         int cranksJustChangedFlag = 0;
         auto currentMillis = std::chrono::system_clock::now();
         unsigned long millisecondsElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(currentMillis - startMillis).count();
-        unsigned short lowMilliBit = millisecondsElapsed % 256;
-        unsigned short highMilliBit = millisecondsElapsed / 256 % 256; // make sure it isn't over 255 and just let it overflow
         unsigned short lowRevBit = numberOfRevolutions % 256;
         unsigned short midRevBit = numberOfRevolutions / 256 % 256;
         unsigned short highRevBit = numberOfRevolutions / (256 * 256);
+        unsigned short lowCrankBit = numberOfCranks % 256;
+        unsigned short highCrankBit = numberOfCranks / 256 % 256; // make sure it isn't over 255 and just let it overflow
         unsigned long millisecondsElapsedSinceLastReporting = std::chrono::duration_cast<std::chrono::milliseconds>(currentMillis - lastReportingTimeInMillis).count();
         QByteArray value;
 
@@ -107,37 +113,52 @@ int main(int argc, char *argv[])
             numberOfRevolutions += 1;
             revsJustChangedFlag = 1;
             lastWheelValue = HIGH;
+            lowWheelMilliBit = millisecondsElapsed % 256;
+            highWheelMilliBit = millisecondsElapsed / 256 % 256; // make sure it isn't over 255 and just let it overflow
         }
         else
         {
             lastWheelValue = wheelSensorValue;
         }
+        if (crankSensorValue == HIGH && lastCrankValue != HIGH)
+        {
+            numberOfCranks += 1;
+            cranksJustChangedFlag = 1;
+            lastCrankValue = HIGH;
+            lowCrankMilliBit = millisecondsElapsed % 256;
+            highCrankMilliBit = millisecondsElapsed / 256 % 256; // make sure it isn't over 255 and just let it overflow
+        }
+        else
+        {
+            lastCrankValue = crankSensorValue;
+        }
         wheelSensorValue = digitalRead(WHEEL_SENSOR_PIN);
-        value.append(char(3));            // required for csc data 1=wheel, 2=crank, 3=both
-                                          // ************************************
-                                          // WHEEL REVOLUTION DATA uint32
-        value.append(char(lowRevBit));    // low bit of revolutions
-        value.append(char(midRevBit));    // mid1 bit of revolutions
-        value.append(char(highRevBit));   // mid2 bit of revolutions
-        value.append(char(0));            // high bit of revolutions.  Not bothering
-                                          // ************************************
-                                          // WHEEL TIME DATA uint16
-        value.append(char(lowMilliBit));  // low bit of milliseconds
-        value.append(char(highMilliBit)); // high bit of milliseconds
-                                          // ************************************
-                                          // CRANK REVOLUTION DATA uint16
-        value.append(char(lowRevBit));    // low bit of revolutions
-        value.append(char(midRevBit));    // mid1 bit of revolutions
-                                          // ************************************
-                                          // CRANK TIME DATA uint16
-        value.append(char(lowMilliBit));  // low bit of milliseconds
-        value.append(char(highMilliBit)); // high bit of milliseconds
-                                          // ************************************
+        crankSensorValue = digitalRead(CRANK_SENSOR_PIN);
+        value.append(char(3));                 // required for csc data 1=wheel, 2=crank, 3=both
+                                               // ************************************
+                                               // WHEEL REVOLUTION DATA uint32
+        value.append(char(lowRevBit));         // low bit of revolutions
+        value.append(char(midRevBit));         // mid1 bit of revolutions
+        value.append(char(highRevBit));        // mid2 bit of revolutions
+        value.append(char(0));                 // high bit of revolutions.  Not bothering
+                                               // ************************************
+                                               // WHEEL TIME DATA uint16
+        value.append(char(lowWheelMilliBit));  // low bit of milliseconds
+        value.append(char(highWheelMilliBit)); // high bit of milliseconds
+                                               // ************************************
+                                               // CRANK REVOLUTION DATA uint16
+        value.append(char(lowCrankBit));       // low bit of revolutions
+        value.append(char(highCrankBit));      // mid1 bit of revolutions
+                                               // ************************************
+                                               // CRANK TIME DATA uint16
+        value.append(char(lowCrankMilliBit));  // low bit of milliseconds
+        value.append(char(highCrankMilliBit)); // high bit of milliseconds
+                                               // ************************************
         QLowEnergyCharacteristic characteristic = service->characteristic(QBluetoothUuid::CSCMeasurement);
         Q_ASSERT(characteristic.isValid());
 
         // REPORT IF IT HAS BEEN MORE THAN ONE SECOND OR IF THE REVOLUTIONS JUST INCREASED
-        if (millisecondsElapsedSinceLastReporting > MAX_REPORTING_INTERVAL || revsJustChangedFlag == REVOLUTION_REPORTING_INTERVAL)
+        if (millisecondsElapsedSinceLastReporting > MAX_REPORTING_INTERVAL)
         {
             lastReportingTimeInMillis = currentMillis;
             service->writeCharacteristic(characteristic, value); // Do the notify.
