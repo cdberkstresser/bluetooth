760e8bb5ffd721f689feb15a9a13ca2855d81fc4
Caleb Berkstresser <cdberkstresser@protonmail.com>
2023-03-27 20:49:28 +0100

    initial working running service

--- running-speed-service/main.cpp
+++ running-speed-service/main.cpp
@@ -14,15 +14,21 @@
 #include <QtCore/qtimer.h>
 #include <chrono>
 #include <wiringPi.h>
+#include <cmath>
+
+
+// DISTANCE TRAVELLED IN ON RPM OF THE TREADMILL IN MILLIMETERS
+#define DISTANCE_PER_REVOLUTION 133
 
 int main(int argc, char *argv[])
 {
-    // GPI BCM PIN 3 ACROSS FROM GROUND
-    const int CRANK_SENSOR_PIN = 9;
+
+    // GPI BCM PIN 2
+    const int WHEEL_SENSOR_PIN = 8;
     // CYCLE INTERVAL IN MILLISECONDS
-    const int TIMER_REPORTING_INTERVAL = 2;
+    const int TIMER_REPORTING_INTERVAL = 1;
     // MAXIMUM TIME ALLOWED TO MANUALLY TRIP A NOTIFICATION UPDATE IN MILLISECONDS
-    const int MAX_REPORTING_INTERVAL = 250;
+    const int MAX_REPORTING_INTERVAL = 1000;
     // SET UP WIRING PI CONNECTION
     wiringPiSetup();
 
@@ -32,13 +38,13 @@ int main(int argc, char *argv[])
     QLowEnergyAdvertisingData advertisingData;
     advertisingData.setDiscoverability(QLowEnergyAdvertisingData::DiscoverabilityGeneral);
     advertisingData.setIncludePowerLevel(true);
-    advertisingData.setLocalName("Berkstresser_Running");
+    advertisingData.setLocalName("Golds410");
     advertisingData.setServices(QList<QBluetoothUuid>() << QBluetoothUuid::RunningSpeedAndCadence);
 
     // SET UP CHARACTERISTIC DATA FOR SPEED MEASUREMENT
     QLowEnergyCharacteristicData rscMeasurementData;
     rscMeasurementData.setUuid(QBluetoothUuid::RSCMeasurement);
-    rscMeasurementData.setValue(QByteArray(2, 0));
+    rscMeasurementData.setValue(QByteArray(5, 0));
     rscMeasurementData.setProperties(QLowEnergyCharacteristic::Notify);
     const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::ClientCharacteristicConfiguration,
                                                 QByteArray(2, 0));
@@ -48,10 +54,12 @@ int main(int argc, char *argv[])
     QLowEnergyCharacteristicData rscDescriptionData;
     rscDescriptionData.setUuid(QBluetoothUuid::RSCFeature);
     QByteArray rscDescriptionBytes;
-    // first value is the "Wheel Revolution Data Present" flag
+    // first value is the "Instantaneous Stride Length Present" flag
     rscDescriptionBytes.append(char(0));
-    // second value is the "Crank Revolution Data Present" flag
+    // second value is the "Total Distance Present" flag
     rscDescriptionBytes.append(char(0));
+    // third value is the "Walking or Running Status" bits" flag (0 = walking, 1 = running)
+    // rscDescriptionBytes.append(char(0));
     rscDescriptionData.setValue(rscDescriptionBytes);
     rscDescriptionData.setProperties(QLowEnergyCharacteristic::Read);
 
@@ -71,75 +79,57 @@ int main(int argc, char *argv[])
     // DEFINE THE TIMER THAT WILL QUERY THE HALL SENSOR FOR PIN CHANGES
     QTimer cyclingServiceLoop;
     auto startMillis = std::chrono::system_clock::now();
-    pinMode(CRANK_SENSOR_PIN, INPUT);
+    pinMode(WHEEL_SENSOR_PIN, INPUT);
 
-    int lastCrankValue = digitalRead(CRANK_SENSOR_PIN);
-    unsigned int numberOfRevolutions = 0;
-    unsigned int numberOfCranks = 0;
-    int crankSensorValue = lastCrankValue;
-    unsigned short lowCrankMilliBit = 1;
-    unsigned short highCrankMilliBit = 0;
+    int lastWheelValue = digitalRead(WHEEL_SENSOR_PIN);
+    unsigned int numberOfRevolutionsSinceLastReporting = 0;
+    int wheelSensorValue = lastWheelValue;
+    unsigned short lowWheelMilliBit = 1;
+    unsigned short highWheelMilliBit = 0;
     auto lastReportingTimeInMillis = std::chrono::system_clock::now();
-    const auto cyclingServiceProvider = [&service, &startMillis, &crankSensorValue, &numberOfRevolutions, &numberOfCranks, &lastCrankValue, &lastReportingTimeInMillis, &lowCrankMilliBit, &highCrankMilliBit]() {
+    const auto cyclingServiceProvider = [&service, &startMillis, &wheelSensorValue, &numberOfRevolutionsSinceLastReporting, &lastWheelValue, &lastReportingTimeInMillis, &lowWheelMilliBit, &highWheelMilliBit]()
+    {
         auto currentMillis = std::chrono::system_clock::now();
-        unsigned long millisecondsElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(currentMillis - startMillis).count();
-        unsigned short lowCrankBit = numberOfCranks % 256;
-        unsigned short highCrankBit = numberOfCranks / 256 % 256; // make sure it isn't over 255 and just let it overflow
         unsigned long millisecondsElapsedSinceLastReporting = std::chrono::duration_cast<std::chrono::milliseconds>(currentMillis - lastReportingTimeInMillis).count();
+
         QByteArray value;
 
-        if (crankSensorValue == HIGH && lastCrankValue != HIGH)
+        if (wheelSensorValue == HIGH && lastWheelValue != HIGH)
         {
-            numberOfCranks += 1;
-            lastCrankValue = HIGH;
-            lowCrankMilliBit = millisecondsElapsed % 256;
-            highCrankMilliBit = millisecondsElapsed / 256 % 256; // make sure it isn't over 255 and just let it overflow
+            numberOfRevolutionsSinceLastReporting += 1;
+            lastWheelValue = HIGH;
         }
         else
         {
-            lastCrankValue = crankSensorValue;
+            lastWheelValue = wheelSensorValue;
         }
-        crankSensorValue = digitalRead(CRANK_SENSOR_PIN);
-        value.append(char(0));                 // required for rsc data 1=wheel, 2=crank, 3=both
-                                               // ************************************
-//                                               // WHEEL REVOLUTION DATA uint32
-//        value.append(char(lowRevBit));         // low bit of revolutions
-//        value.append(char(midRevBit));         // mid1 bit of revolutions
-//        value.append(char(highRevBit));        // mid2 bit of revolutions
-//        value.append(char(0));                 // high bit of revolutions.  Not bothering
-//                                               // ************************************
-//                                               // WHEEL TIME DATA uint16
-//        value.append(char(lowWheelMilliBit));  // low bit of milliseconds
-//        value.append(char(highWheelMilliBit)); // high bit of milliseconds
-                                               // ************************************
-                                               // CRANK REVOLUTION DATA uint16
-//        value.append(char(lowCrankBit));       // low bit of revolutions
-//        value.append(char(highCrankBit));      // mid1 bit of revolutions
-                                               // ************************************
-                                               // CRANK TIME DATA uint16
-//        value.append(char(lowCrankMilliBit));  // low bit of milliseconds
-//        value.append(char(highCrankMilliBit)); // high bit of milliseconds
-                                               // ************************************
-				               // INSTANTANEOUS SPEED uint16 
-	value.append(char(83));		       // low bit of m/s 
-	value.append(char(1));		       // high bit of m/s 
-        				       // INSTANTANEOUS CADENCE uint8
-        value.append(char(0));    	       // value in steps per minute
-		
-	QLowEnergyCharacteristic characteristic = service->characteristic(QBluetoothUuid::RSCMeasurement);
+        wheelSensorValue = digitalRead(WHEEL_SENSOR_PIN);
+
+        QLowEnergyCharacteristic characteristic = service->characteristic(QBluetoothUuid::RSCMeasurement);
         Q_ASSERT(characteristic.isValid());
 
         // REPORT IF IT HAS BEEN MORE THAN ONE SECOND OR IF THE REVOLUTIONS JUST INCREASED
-        if (millisecondsElapsedSinceLastReporting > MAX_REPORTING_INTERVAL)
+        if (millisecondsElapsedSinceLastReporting > MAX_REPORTING_INTERVAL || numberOfRevolutionsSinceLastReporting == 10)
         {
+            double wholeMetersPerSecond;
+            double fractionalMetersPerSecond = modf(double(numberOfRevolutionsSinceLastReporting) * DISTANCE_PER_REVOLUTION / millisecondsElapsedSinceLastReporting, &wholeMetersPerSecond);
+
+            // ************************************
+            // WHEEL REVOLUTION DATA
+            value.append(char(0));   // no idea what this represents - spacer
+            value.append(char(short(fractionalMetersPerSecond * 256))); // speed - m/s - fractional portion * 256
+            value.append(char(short(wholeMetersPerSecond)));    // speed m/s
+            value.append(char(0));   // cadence
             lastReportingTimeInMillis = currentMillis;
+            numberOfRevolutionsSinceLastReporting = 0;
             service->writeCharacteristic(characteristic, value); // Do the notify.
         }
     };
     QObject::connect(&cyclingServiceLoop, &QTimer::timeout, cyclingServiceProvider);
     cyclingServiceLoop.start(TIMER_REPORTING_INTERVAL);
 
-    auto reconnect = [&leController, advertisingData, &service, serviceData]() {
+    auto reconnect = [&leController, advertisingData, &service, serviceData]()
+    {
         service.reset(leController->addService(serviceData));
         if (!service.isNull())
             leController->startAdvertising(QLowEnergyAdvertisingParameters(),
@@ -149,4 +139,3 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
-
